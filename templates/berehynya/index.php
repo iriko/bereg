795c2d4ee1a8823630d4419e892c1580efa85720
iriko <yarik.kuzmenko@gmail.com>
2013-04-04 11:29:15 +0300

    Зміна шаблону та налаштування стилів

--- templates/berehynya/index.php
+++ templates/berehynya/index.php
@@ -13,42 +13,58 @@ JHtml::_('behavior.framework', true);
 /* The following line gets the application object for things like displaying the site name */
 $doc = JFactory::getDocument();
 $doc->addStyleSheet($this->baseurl.'/templates/'.$this->template.'/css/template.css', $type = 'text/css', $media = 'screen,projection');
+$this->setGenerator('Beregynya site');
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
 	<head>
         <jdoc:include type="head" />
+        <script type="text/javascript">
+            jQuery(document).ready(function() {
+                //Для все картинок на сайте
+                jQuery("img").removeAttr("title");
+                //Добавляем клас sub-menu
+                jQuery('.header-menu .deeper > ul').addClass('sub-menu');
+            })
+        </script>
     </head>
 
 	<body>
-        <div class="page-holder">
+        <div class="wrapper-holder">
 
             <div class="header-holder">
                 <div class="header">
+
                     <div class="logo"><a href="<?php echo $this->baseurl ?>/"><img src="<?php echo $this->baseurl.'/templates/'.$this->template.'/images/logo.jpg'?>" alt="Бегериня"></a></div>
-                    <div class="header-menu">
-                        <jdoc:include type="modules" name="position-1" />
-                    </div>
+
+                    <jdoc:include type="modules" name="position-1" />
+
                 </div>
             </div><!--header-holder-->
 
-            <div class="content-holder">
+            <div class="page-holder">
+                <div class="content-holder">
 
-                <jdoc:include type="modules" name="position-2" style="home"/>
+                    <jdoc:include type="modules" name="position-2" style="home"/>
 
-                <div class="content-area">
-                    <jdoc:include type="message" />
-                    <jdoc:include type="component" />
+                    <div class="content-area">
+                        <jdoc:include type="message" />
+                        <jdoc:include type="component" />
+                    </div>
+
                 </div>
-
-            </div><!--content-holder-->
+            </div><!--page-holder-->
 
             <div class="footer-holder">
-                <div class="footer-menu">
+                <div class="footer">
+
+                    <div class="copyright">Черкаський колегіум "Бегериня" <?php echo date('Y') ?></div>
+
                     <jdoc:include type="modules" name="position-3" />
+
                 </div>
             </div><!--footer-holder-->
 
-        </div><!--page-holder-->
+        </div><!--wrapper-holder-->
     </body>
 </html>
